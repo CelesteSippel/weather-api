9da9673edcd75c72e016f1e2451460ba7a3b9f5d
Celeste Sippel <celestechristine@gmail.com>
2019-11-12 16:15:35 -0500

    weather api complete

--- public/main.js
+++ public/main.js
@@ -1,7 +1,23 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let weatherData
+
+const getWeather = async () => {
+  const city = document.querySelector('input').value
+  console.log('going out to api')
+  const resp = await fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+      city +
+      '&appid=c65edb846e1622f0daec1fe4f480a96f&units=imperial'
+  )
+  console.log('back from api')
+  weatherData = await resp.json()
+  // console.log(weatherData)
+
+  displayData(weatherData.weather[0].main + ', ' + weatherData.main.temp + 'F')
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const displayData = weatherData => {
+  document.querySelector('.weather-results').textContent = weatherData
+}
+document.querySelector('.search-button').addEventListener('click', getWeather)
+
+document.querySelector('.weather-results').textContent = weatherData
